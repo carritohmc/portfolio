ea2395fd3f33cf4d38677a20da61e4dd47200c23
carritohmc <carritohmc@gmail.com>
2023-08-13 12:49:30 -0400

    Updated pages and css

---
+++ js/interaction.js
@@ -0,0 +1,12 @@
+function menuToggle() {
+
+    let menu= document.getElementById("nav-mobile");
+
+    if (menu.className == "nav-mobile") {
+        menu.classList.remove("nav-mobile");
+        menu.classList.add("hidden");
+    } else {
+        menu.classList.remove("hidden");
+        menu.classList.add("nav-mobile");
+    }
+}
